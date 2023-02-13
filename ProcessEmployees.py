6ac10e0d78c8bff3064f6fcac98a159502706346
Jiaze Lin <peter_lin2@baylor.edu>
2023-02-13 15:05:57 -0600

    2.13

--- ProcessEmployees.py
+++ ProcessEmployees.py
@@ -1,4 +1,4 @@
-'''
+"""
 Due the outstanding performance of the Customer Service Represetatives (CSRs) in the marketing department, management
 has decided to reward them with a 10% increase in their salary. To evaluate the impact on the budget, you have been
 asked to run a report on the employee file and display the results of BEFORE AND AFTER the salary increase.
@@ -6,36 +6,41 @@ asked to run a report on the employee file and display the results of BEFORE AND
 You will create a dictionary that has the full employee name as the key and ONLY their new salary as the value.
 
 Iternate through the dictionary to print out their name and thier new salary (as in image)
-'''
+"""
 
 import csv
 
-#open the file
+# open the file
 
+in_file = open("employee_data.csv", "r")
+reader = csv.reader(in_file, delimiter=",")
+next(reader)
 
+# create an empty dictionary
+marketing_salary_dict = {}
 
-
-#create an empty dictionary
-
-
-#use a loop to iterate through the csv file
-
-
-    #check if the employee fits the search criteria
-
-
-    
+# use a loop to iterate through the csv file
+for row in reader:
+    current_salary = row[5]
+     # check if the employee fits the search criteria
+    if row[3] == "Marketing":
+        print(
+            "Manager Name: ", row[1] + row[2], "Current Salary: ", "$ " + current_salary
+        )
+        full_name = row[1] + row[2]
+       
 
 print()
-print('=========================================')
+print("=========================================")
 print()
 
-#iternate through the dictionary and print out the key and value as per printout
-
-
-
-          
-        
-
-        
-    
+# iternate through the dictionary and print out the key and value as per printout
+for row in reader:
+    current_salary = row[5]
+     # check if the employee fits the search criteria
+    if row[3] == "Marketing":
+        print(
+            "Manager Name: ", row[1] + row[2], "New Salary: ", "$ " + current_salary
+        )
+        full_name = row[1] + row[2]
+        marketing_salary_dict [full_nam
